02c035fcb3e80d4763ff6c724b82893e03d74df7
Yamanthegreat <105352648+Yamanthegreat@users.noreply.github.com>
2023-04-29 13:53:39 +0530

    Add files via upload

---
+++ mangoes-main/mangoes-main/plucking mangoes2/sketch.js
@@ -0,0 +1,38 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+
+var treeObj, stoneObj,groundObject;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
+var world,boy;
+
+//Declare launcherObject and launchForce variable here
+
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	
+
+	Engine.run(engine);
+}
+
+function draw() {
+
+  background(230);
+  textSize(25);
+  text("Press Space to get a second Chance to Play!!",50 ,50);
+  image(boy ,200,340,200,300);
+  
+
+}
